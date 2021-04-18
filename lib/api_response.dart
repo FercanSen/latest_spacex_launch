390936bc9e0103d4bc1d242fc7a594e8e72662e0
fercansen <fercansen@gmail.com>
2021-04-18 21:55:46 +0300

    Api response for data

--- lib/api_response.dart
+++ lib/api_response.dart
@@ -1,19 +1,68 @@
+import 'dart:convert';
+
+ApiResponse apiResponseFromJson(String str) =>
+    ApiResponse.fromJson(json.decode(str));
+
+String apiResponseToJson(ApiResponse data) => json.encode(data.toString());
+
 class ApiResponse {
   final String name;
   final String details;
-  // final String patch;
 
   ApiResponse({
     required this.name,
     required this.details,
-    // required this.patch,
   });
 
   factory ApiResponse.fromJson(Map<String, dynamic> json) {
     return ApiResponse(
       name: json["name"],
       details: json["details"],
-      // patch: json["patch"],
     );
   }
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "details": details,
+      };
+}
+
+class Links {
+  Patch patch;
+  String webcast;
+
+  Links({
+    required this.patch,
+    required this.webcast,
+  });
+
+  factory Links.fromJson(Map<String, dynamic> json) => Links(
+        patch: Patch.fromJson(json["patch"]),
+        webcast: json["webcast"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "patch": patch.toJson(),
+        "webcast": webcast,
+      };
+}
+
+class Patch {
+  String small;
+  String large;
+
+  Patch({
+    required this.small,
+    required this.large,
+  });
+
+  factory Patch.fromJson(Map<String, dynamic> json) => Patch(
+        small: json["small"],
+        large: json["large"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "small": small,
+        "large": large,
+      };
 }
